68fc6bb00447c18ed51a2a985dabdf6c50384a88
Ilhee <asahi8769@gmail.com>
2020-08-11 13:54:36 +0900

    Apply all changes

---
+++ ckd_main.bat
@@ -0,0 +1,8 @@
+@ECHO ON
+title CKD_AUTO Start
+
+cd /D D:\devs\autom
+
+python ckd_main.py
+
+cmd.exe
---
+++ git_.bat
@@ -0,0 +1,8 @@
+@ECHO ON
+title CKD_AUTO_gituploader Start
+
+cd /D D:\devs\autom
+
+python git_.py
+
+cmd.exe
